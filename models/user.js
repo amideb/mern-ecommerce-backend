a9e8a73abe0219af52ceed0f6bdb50ead9fc88b2
Debrup Mondal <debrup.official.365@gmail.com>
2021-03-05 15:24:30 +0530

    user schema updated

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
+const crypto = require("crypto");
+const uuidv1 = require("uuid/v1");
+
 const userSchema = new Schema({
   name: {
     type: String,
@@ -26,11 +29,9 @@ const userSchema = new Schema({
     trim: true,
   },
 
-  //TODO : Come back here
-
   encry_password: {
     type: String,
-    required:true
+    required: true,
   },
   salt: String,
 
@@ -45,21 +46,37 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.virtual("password")
+           .set(function(password){
+               this._password= password
+               this.salt= uuidv1();
+               this.encry_password = this.securePassword(password)
+           })
+           .get(function(){
+               return this._password;
+           });
 
-userSchema.method={
+userSchema.method = {
 
-    securePassword: function(plainpassword){
-        if(!password) return "";
-        try{
-            return crypto.createHmac('sha256', this.salt)
-            .update(plainpassword)
-            .digest('hex');
+    authenticate : function(plainpassword){
+        return this.securePassword(plainpassword)===this.encry_password;
+    },
 
-        } catch(err){
-            return "";
-        }
+
+
+
+
+  securePassword: function (plainpassword) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
     }
-
-}
+  },
+};
 
 module.exports = mongoose.model("User", userSchema);
